e2464c587f076acb3ab8e71c185c393ada45b09f
wliang002 <wliang002@berkeley.edu>
2023-01-17 03:46:17 -0600

    add tabbar and two vcs

---
+++ Album Finder/FavoritesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewController.swift
+//  Album Finder
+//
+//  Created by Emily Liang on 1/15/23.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Album Finder/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Album Finder
+//
+//  Created by Emily Liang on 1/15/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
